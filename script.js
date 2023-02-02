ecc094597447649b5166e76e1cf3a4ccd27f9335
daslopes <daslopes@outlook.com.br>
2023-02-02 20:43:15 -0300

    salvando exercicio

---
+++ script.js
@@ -0,0 +1,6 @@
+function mostrar(){
+    let entrada = window.document.querySelector('input#entrada').value
+let saida = window.document.querySelector('section#saida')
+saida.innerHTML = entrada
+}
+
